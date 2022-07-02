ba8930d96b96e3195cbf82575234e4a1b29ceb81
Marcos Sandoval <marcosbancaprepa@gmail.com>
2022-07-02 11:43:39 -0600

    Update vite.config.js

--- react-rick-and-morty/vite.config.js
+++ react-rick-and-morty/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://marcos1394.github.io/React-Rick-Morty-Api",
   plugins: [react()]
 })
